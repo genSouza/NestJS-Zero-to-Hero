c914374986aea020723c2a708d0127a217e7439f
Genilton Souza <genilton.souza26@gmail.com>
2020-05-07 17:36:21 -0400

    lesson 33

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -24,7 +24,7 @@ export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getTasks(@Query() filterDto: GetTaskFilterDto): Task[] {
+  getTasks(@Query(ValidationPipe) filterDto: GetTaskFilterDto): Task[] {
     if (Object.keys(filterDto).length) {
       return this.tasksService.getTaskWithFilters(filterDto);
     }
