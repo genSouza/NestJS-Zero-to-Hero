1d72322b182de1852f36cd0e2e6005fc8a24df5e
Genilton Souza <genilton.souza26@gmail.com>
2020-05-07 09:51:16 -0400

    lesson 16

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,14 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
-    /**
-     *
-     */
-    constructor(private tasksService: TasksService) {
-        
-        
-    }
+  /**
+   *
+   */
+  constructor(private tasksService: TasksService) {}
 
+  @Get()
+  getAllTasks() {
+    this.tasksService.getAllTasks();
+  }
 }
