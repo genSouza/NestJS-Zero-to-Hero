863c232c86bc03b9180e8c0d86e91a76022332be
Genilton Souza <genilton.souza26@gmail.com>
2020-05-11 11:57:48 -0400

    lesson 51

---
+++ src/auth/user.entity.ts
@@ -0,0 +1,14 @@
+import { BaseEntity, PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    username: string;
+
+    @Column()
+    password: string;
+}
