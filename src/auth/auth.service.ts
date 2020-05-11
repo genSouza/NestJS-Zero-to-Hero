a9ffd174d8ffa3644fd6830c680345a578cf1fbe
Genilton Souza <genilton.souza26@gmail.com>
2020-05-11 14:14:51 -0400

    lesson 52

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,10 +1,15 @@
 import { Injectable } from '@nestjs/common';
 import { UserRepository } from './user.repository';
 import { InjectRepository } from '@nestjs/typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository) private userRepository: UserRepository,
   ) {}
+
+    async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+        return await this.userRepository.signUp(authCredentialsDto);
+  }
 }
--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -1,8 +1,17 @@
 import { Repository, EntityRepository } from 'typeorm';
 import { User } from './user.entity';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
+import { v4 as uuidv4 } from 'uuid';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
-    
-    
+  async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentialsDto;
+
+    const user = new User();
+    user.username = username;
+    user.password = password;
+    user.uuid = uuidv4();
+    await user.save();
+  }
 }
